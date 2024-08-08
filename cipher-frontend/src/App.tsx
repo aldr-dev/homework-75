7bd9084ed478ccff9452f989d66e78015dad26fe
Gordus Alexander <aldrmiller@gmail.com>
2024-08-08 23:52:50 +0600

    инициализировал приложение на react + typescript + express

---
+++ cipher-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      Home
+    </>
+  );
+};
+
+export default App;
---
+++ cipher-frontend/src/main.tsx
@@ -0,0 +1,19 @@
+import App from './App';
+import {createRoot} from 'react-dom/client';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+// import {Provider} from 'react-redux';
+import {ToastContainer} from 'react-toastify';
+import theme from './theme';
+import 'react-toastify/dist/ReactToastify.css';
+
+createRoot(document.getElementById('root')!).render(
+  <>
+    {/*<Provider store={store}>*/}
+    <ToastContainer position="bottom-right" theme="light" autoClose={3000}/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <App/>
+    </ThemeProvider>
+    {/*</Provider>*/}
+  </>
+);
